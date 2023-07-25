1462f61dd5119e09727aac5d79d0154fce2667e2
pankajforsnowflake1989 <140538778+pankajforsnowflake1989@users.noreply.github.com>
2023-07-25 22:51:38 +0530

    Update streamlit_app.py
    created user intractive list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('🥗Breakfast Menu')
+streamlit.header('🥗Breakfast Favourites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothy')
 streamlit.text('🐔 🍞Hard-Boiled Free-Range Egg')
@@ -11,5 +11,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
   
